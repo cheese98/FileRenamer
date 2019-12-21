4df8bf44e2546b145ef000b5a874db1569e7a02d
cheese98 <cheesej98@gmail.com>
2019-12-21 16:20:18 +0900

    added listBox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.ApplyButton = new System.Windows.Forms.Button();
+            this.applyButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
             // 
+            this.listBox1.AllowDrop = true;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 12;
             this.listBox1.Location = new System.Drawing.Point(12, 12);
@@ -41,22 +43,33 @@
             this.listBox1.Size = new System.Drawing.Size(258, 424);
             this.listBox1.TabIndex = 0;
             // 
-            // ApplyButton
+            // applyButton
             // 
-            this.ApplyButton.Location = new System.Drawing.Point(632, 391);
-            this.ApplyButton.Name = "ApplyButton";
-            this.ApplyButton.Size = new System.Drawing.Size(156, 47);
-            this.ApplyButton.TabIndex = 1;
-            this.ApplyButton.Text = "적용";
-            this.ApplyButton.UseVisualStyleBackColor = true;
-            this.ApplyButton.Click += new System.EventHandler(this.button1_Click);
+            this.applyButton.Location = new System.Drawing.Point(632, 391);
+            this.applyButton.Name = "applyButton";
+            this.applyButton.Size = new System.Drawing.Size(156, 47);
+            this.applyButton.TabIndex = 1;
+            this.applyButton.Text = "적용";
+            this.applyButton.UseVisualStyleBackColor = true;
+            this.applyButton.Click += new System.EventHandler(this.applyButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(313, 12);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 2;
+            this.removeButton.Text = "제거";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.ApplyButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.applyButton);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -67,7 +80,8 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button ApplyButton;
+        private System.Windows.Forms.Button applyButton;
+        private System.Windows.Forms.Button removeButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,11 +15,32 @@ namespace FileRenamer
         public Form1()
         {
             InitializeComponent();
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+        }
+        private void listBox1_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+        private void listBox1_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            int i;
+            for (i = 0; i < s.Length; i++)
+                listBox1.Items.Add(s[i]);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void applyButton_Click(object sender, EventArgs e)
         {
+            Apply.ApplyChanges();
+        }
 
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
         }
     }
 }
